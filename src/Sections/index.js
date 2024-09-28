59d5fa882c950b510578c07edc304be7cd683a91
Aman Kumar Gupta <129995427+Aman-kr477@users.noreply.github.com>
2024-09-28 22:20:16 +0530

    Add files via upload

---
+++ src/Sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import PopularProducts from "./PopularProduct";
+
+
+export{
+    Hero,
+    CustomerReviews,
+    Services,
+    SpecialOffer,
+    Footer,
+    SuperQuality,
+    Subscribe,
+    PopularProducts,
+  
+};
